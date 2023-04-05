0a33856957730caa71d653ed7b24be175a93c72c
Cordoba Leandro <cordobaleandro000@gmail.com>
2023-04-05 13:43:44 -0300

    Realizados los apartados del punto 3, Ejercicio 2

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 void main(){
-    printf("Hola mundo");
+    //printf("Hola mundo");
+    int x = 10;
+    int *puntX = &x;
+    printf("El contenido del puntero: %d", *puntX);
+    printf("\nLa dirección de memoria almacenada por el puntero: %p", puntX);
+    printf("\nLa dirección de memoria de la variable: %p", &x);
+    printf("\nLa dirección de memoria del puntero: %p", &puntX);
+    printf("\nEl tamaño de memoria utilizado por x: %d", sizeof(x));
 }
